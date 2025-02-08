2f51941f27fcc9d88b76ebef6f8261458b75ef59
JavierJimenezIE <javier.jimenez@derilinx.com>
2025-02-08 16:30:58 +0000

    initialize Django project with API structure and models for quotes, authors, and users...

---
+++ backend/api/serializers.py
@@ -0,0 +1,80 @@
+# api/serializers.py
+from rest_framework import serializers
+from .models import (
+    User, Author, Book, Tag, Quote, QuoteTag,
+    QuoteGroup, QuoteGroupMembership, QuoteGroupShare,
+    QuoteList, QuoteListQuote, Document, ImportLog
+)
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)  # This hashes the password
+        user.save()
+        return user
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+class QuoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quote
+        fields = '__all__'
+
+class QuoteTagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuoteTag
+        fields = '__all__'
+
+class QuoteGroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuoteGroup
+        fields = '__all__'
+
+class QuoteGroupMembershipSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuoteGroupMembership
+        fields = '__all__'
+
+class QuoteGroupShareSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuoteGroupShare
+        fields = '__all__'
+
+class QuoteListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuoteList
+        fields = '__all__'
+
+class QuoteListQuoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuoteListQuote
+        fields = '__all__'
+
+class DocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Document
+        fields = '__all__'
+
+class ImportLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ImportLog
+        fields = '__all__'
---
+++ backend/api/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    UserViewSet, AuthorViewSet, BookViewSet, TagViewSet, QuoteViewSet, QuoteTagViewSet,
+    QuoteGroupViewSet, QuoteGroupMembershipViewSet, QuoteGroupShareViewSet,
+    QuoteListViewSet, QuoteListQuoteViewSet, DocumentViewSet, ImportLogViewSet
+)
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'authors', AuthorViewSet)
+router.register(r'books', BookViewSet)
+router.register(r'tags', TagViewSet)
+router.register(r'quotes', QuoteViewSet)
+router.register(r'quote-tags', QuoteTagViewSet)
+router.register(r'quote-groups', QuoteGroupViewSet)
+router.register(r'group-memberships', QuoteGroupMembershipViewSet)
+router.register(r'group-shares', QuoteGroupShareViewSet)
+router.register(r'quote-lists', QuoteListViewSet)
+router.register(r'quote-list-quotes', QuoteListQuoteViewSet)
+router.register(r'documents', DocumentViewSet)
+router.register(r'import-logs', ImportLogViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
